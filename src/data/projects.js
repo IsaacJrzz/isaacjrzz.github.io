9aed5c0558f9d5c089e7c68c2c0a966fc806947c
ALONSO JUAREZ <iajuarez@segulagrp.es>
2026-03-26 08:48:27 +0100

    update

--- src/data/projects.js
+++ src/data/projects.js
@@ -19,15 +19,6 @@ export const projectsData = [
     descriptionLeft: "Proyecto de Fin de Grado (TFG) enfocado en solucionar la fragmentación digital de SaludMadrid. Una propuesta que unifica Cita Sanitaria y Tarjeta Virtual en una única interfaz multiplataforma optimizada.",
     descriptionRight: "Actualmente en fase de desarrollo, el sistema integra un módulo de Salud Mental y arquitectura UML avanzada. El proyecto abarca desde el análisis legal hasta el prototipado de alta fidelidad validado."
 },
-{
-    "number": "03",
-    "title": "ALPHA",
-    "type": "SIMULACIÓN • DEVOPS • JS",
-    "images": ["formacion-fundaciones.png"],
-    "link": "https://github.com/IsaacJrzz/Plat-Alpha",
-    "external": true,
-    "descriptionLeft": "Motor de simulación incremental enfocado en el ciclo de vida del software. El sistema permite gestionar desde la generación manual de código hasta el despliegue de infraestructuras críticas autónomas.",
-    "descriptionRight": "Desarrollado con un motor de eventos asíncrono, incluye mecánicas de prestigio (Deploy), gestión de errores en tiempo real (Bugs) y un sistema visual dinámico basado en sintaxis Monokai con control de densidad."
-}, 
+
   // ... otros proyectos
 ];
